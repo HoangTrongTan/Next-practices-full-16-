80f1d9f69689c7c46fed6d08458976e96199610f
HoangTrongTan <hoangtrongtan15112000@gmail.com>
2026-01-04 16:04:48 +0700

    update blog expception using try/catch

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,15 +3,21 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 
 const revalidate = 50; //tăng hiệu suất bằng cách cache data trong 50s
 export default async function Blog() {
-    const res = await fetch(`${apiUrl}/blog`,{
-        next: {revalidate} //ssr server side rendering lấy data theo chu kỳ cứ sau 50s 1 lần gọi data
+  let data: { posts: any[] } = { posts: [] };
+  try {
+    const res = await fetch(`${apiUrl}/blog`, {
+      next: { revalidate }, //ssr server side rendering lấy data theo chu kỳ cứ sau 50s 1 lần gọi data
     });
-    const data = await res.json();
+    data = await res.json();
     console.log("Blog data:", data);
-    
-    return <div className="bg-teal-50 h-[calc(100vh)] p-10">
-        
-        {/* Client Comp */}
-        <BlogPage data={data.posts} />
+  } catch (err) {
+    data = { posts: [] };
+  }
+
+  return (
+    <div className="bg-teal-50 h-[calc(100vh)] p-10">
+      {/* Client Comp */}
+      <BlogPage data={data.posts} />
     </div>
+  );
 }
